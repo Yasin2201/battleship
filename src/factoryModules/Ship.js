92645a580390109b74e7cfb658e80132d517a378
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-06-06 20:09:09 +0100

    added hit func

--- src/factoryModules/Ship.js
+++ src/factoryModules/Ship.js
@@ -1,6 +1,12 @@
 const Ship = (shipLength) => {
     const length = shipLength
     const shipHit = []
+
+    const hit = (hitCoords) => {
+        shipHit.push(hitCoords)
+    }
+
+    return { length, shipHit, hit }
 }
 
 export default Ship
