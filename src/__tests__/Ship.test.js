62303cb62efe31e08ae0831e9cc2b499208861d9
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-06-06 20:09:43 +0100

    added tests for Ship

--- src/__tests__/Ship.test.js
+++ src/__tests__/Ship.test.js
@@ -1,5 +1,26 @@
 import Ship from '../factoryModules/Ship'
 
-test('test', () => {
-    expect(Ship(1)).toBe(1)
+const ship1 = Ship(2)
+
+test('ship length', () => {
+    expect(ship1.length).toBe(2)
+})
+
+test('shipHit Array Empty', () => {
+    expect(ship1.shipHit).toEqual([])
+})
+
+test('shipHit Array Hit', () => {
+    ship1.hit('A1')
+    expect(ship1.shipHit).toEqual(['A1'])
+})
+
+test('ship isSunk = false', () => {
+    expect(ship1.isSunk()).toBe(false)
+})
+
+test('ship isSunk = true', () => {
+    ship1.hit('A2')
+    expect(ship1.shipHit).toEqual(['A1', 'A2'])
+    expect(ship1.isSunk()).toBe(true)
 })
