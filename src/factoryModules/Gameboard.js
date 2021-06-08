997c655bd1691e3c9145b1ad766aaac04d316da6
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-06-08 21:13:14 +0100

    refactored code

--- src/factoryModules/Gameboard.js
+++ src/factoryModules/Gameboard.js
@@ -1,32 +1,29 @@
 const Gameboard = () => {
-    const boardXaxis = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-    const boardYaxis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const boardXaxis = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const boardYaxis = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     const fullBoard = boardXaxis.map((Xele) => {
-        return boardYaxis.map((Yele) => Xele + Yele)
+        return boardYaxis.map((Yele) => `${Xele}${Yele}`)
     })
 
     const placedShips = []
 
-    const placeShip = (ship, index, placeStart) => {
-        const shipStartPos = fullBoard[index].indexOf(placeStart)
-
-        if (shipStartPos + ship.length > 10) {
+    const placeShip = (ship, indexY, indexX) => {
+        if (indexX + ship.length > 10) {
             throw new Error("Ship Position OverBoard!");
         } else {
-            const shipsFullPos = fullBoard[index].slice(shipStartPos, shipStartPos + ship.length)
+            const shipsFullPos = fullBoard[indexY].slice(indexX, indexX + ship.length)
             ship.shipCoords = shipsFullPos
             placedShips.push(ship)
         }
     }
 
-    const receiveAttack = (index, attackCoords) => {
-        const foundShip = placedShips.find((ship) => ship.shipCoords.includes(attackCoords))
+    const receiveAttack = (indexY, indexX) => {
+        const foundShip = placedShips.find((ship) => ship.shipCoords.includes(fullBoard[indexY][indexX]))
 
         if (foundShip === undefined) {
-            const missedTargetIndex = fullBoard[index].indexOf(attackCoords)
-            return fullBoard[index].splice(missedTargetIndex, 1, 'Miss')
+            return fullBoard[indexY].splice(indexX, 1, 'Miss')
         } else {
-            return foundShip.hit(attackCoords)
+            return foundShip.hit(fullBoard[indexY][indexX])
         }
     }
 
