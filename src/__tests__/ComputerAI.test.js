48431fc148057c6b76c74726154eaa6f6f4cd4a5
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-06-11 16:14:14 +0100

    test computer send random attack to opp gameboard

--- src/__tests__/ComputerAI.test.js
+++ src/__tests__/ComputerAI.test.js
@@ -1,10 +1,21 @@
 import Gameboard from '../factoryModules/Gameboard'
 import ComputerAI from '../factoryModules/ComputerAI'
 
+
 test('Computer AI randomly place a ship in fleet', () => {
     const gameboard = Gameboard()
-    const comp = ComputerAI(gameboard)
+    const comp = ComputerAI(null, gameboard)
     comp.placeFleet()
     const isFleetPlaced = comp.fleet.every((ship) => ship.shipCoords.length > 0)
+
     expect(isFleetPlaced).toBeTruthy()
+})
+
+test('ComputerAI sends attack to Opponents Gameboard', () => {
+    const humanGB = Gameboard()
+    const compGB = Gameboard()
+    const comp = ComputerAI(humanGB, compGB)
+    const attack = comp.attackOpp()
+
+    expect(humanGB.fullBoard[attack[0]][attack[1]]).toBe('Miss')
 })
