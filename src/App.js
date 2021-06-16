9efb49963bca4a0d2bf73229dc2df6a05135afb9
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-06-16 21:39:15 +0100

    attack cpu board and update display

--- src/App.js
+++ src/App.js
@@ -1,32 +1,59 @@
 import { useState } from 'react';
 import Gameboard from '../src/factoryModules/Gameboard'
-// import Ship from '../src/factoryModules/Ship'
+import Ship from '../src/factoryModules/Ship'
 import ComputerAI from '../src/factoryModules/ComputerAI'
 import DisplayGameboard from './displayComponents/DisplayGameboard';
 import DisplayShipYard from './displayComponents/DisplayShipYard';
 import Gameloop from './factoryModules/Gameloop';
+import './displayComponents/styles/App.css'
 
 function App() {
-  // const [turn, setTurn] = useState(1)
   const [humanGB, setHumanGB] = useState(() => Gameboard())
-  // const humanGB = Gameboard()
-  const cpuGB = Gameboard()
-  const cpu = ComputerAI(humanGB, cpuGB)
-  cpu.placeFleet()
+  const [cpuGB, setCpuGB] = useState(() => Gameboard())
 
+  const cpu = ComputerAI(humanGB, cpuGB)
 
   const check = () => {
     Gameloop(cpu, humanGB)
     setHumanGB({ ...humanGB }, humanGB.fullBoard)
     console.log(humanGB)
+    console.log(cpuGB)
+  }
 
+  const start = () => {
+    setHumanGB({ ...humanGB }, humanGB.placeShip(Ship(4), 0, 0))
+    setHumanGB({ ...humanGB }, humanGB.placeShip(Ship(3), 1, 0))
+    setHumanGB({ ...humanGB }, humanGB.placeShip(Ship(2), 2, 0))
+
+    setCpuGB({ ...cpuGB }, cpu.placeFleet())
+    console.log(cpuGB)
+  }
+
+  const attack = (e) => {
+    let x = e.target.textContent
+    let y = parseInt(x[0])
+    let z = parseInt(x[1])
+
+    if (x === 'Hit' || x === "Miss") {
+      console.log('try again')
+    } else {
+      setHumanGB({ ...humanGB }, humanGB.fullBoard)
+      setCpuGB({ ...cpuGB }, cpuGB.receiveAttack(y, z))
+      Gameloop(cpu, humanGB)
+    }
+
+    // console.log(cpuGB)
   }
 
   return (
     <div>
       <h1>BattleShip</h1>
       <button onClick={check}>Placed Fleet?</button>
-      <DisplayGameboard board={humanGB.fullBoard} />
+      <button onClick={start}>Start</button>
+      <div className='activeBoards'>
+        <DisplayGameboard board={humanGB.fullBoard} />
+        <DisplayGameboard board={cpuGB.fullBoard} attack={attack} />
+      </div>
       <DisplayShipYard />
     </div>
 
