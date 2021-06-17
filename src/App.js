fc20a3e8613c745a2d3674981eab761c8abf51ae
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-06-17 20:59:32 +0100

    added drag/drop to Create and Place human player ships

--- src/App.js
+++ src/App.js
@@ -10,51 +10,66 @@ import './displayComponents/styles/App.css'
 function App() {
   const [humanGB, setHumanGB] = useState(() => Gameboard())
   const [cpuGB, setCpuGB] = useState(() => Gameboard())
+  const [shipLength, setShipLength] = useState(undefined)
+  const [shipLenData, setShipLenData] = useState(Array(5).fill(1))
 
   const cpu = ComputerAI(humanGB, cpuGB)
 
-  const check = () => {
-    Gameloop(cpu, humanGB)
-    setHumanGB({ ...humanGB }, humanGB.fullBoard)
-    console.log(humanGB)
-    console.log(cpuGB)
-  }
-
   const start = () => {
-    setHumanGB({ ...humanGB }, humanGB.placeShip(Ship(4), 0, 0))
-    setHumanGB({ ...humanGB }, humanGB.placeShip(Ship(3), 1, 0))
-    setHumanGB({ ...humanGB }, humanGB.placeShip(Ship(2), 2, 0))
-
     setCpuGB({ ...cpuGB }, cpu.placeFleet())
-    console.log(cpuGB)
+    console.log(cpuGB.placedShips)
   }
 
   const attack = (e) => {
-    let x = e.target.textContent
-    let y = parseInt(x[0])
-    let z = parseInt(x[1])
+    let coord = e.target.textContent
+    let yCoord = parseInt(coord[0])
+    let xCoord = parseInt(coord[1])
 
-    if (x === 'Hit' || x === "Miss") {
-      console.log('try again')
-    } else {
+    try {
       setHumanGB({ ...humanGB }, humanGB.fullBoard)
-      setCpuGB({ ...cpuGB }, cpuGB.receiveAttack(y, z))
+      setCpuGB({ ...cpuGB }, cpuGB.receiveAttack(yCoord, xCoord))
       Gameloop(cpu, humanGB)
+    } catch (err) {
+      alert('Already Attack Position Try Again!')
+    }
+  }
+
+  const shipLenCheck = (e) => {
+    setShipLength(parseInt(e.target.textContent))
+  }
+
+  const dropPlace = (e) => {
+    try {
+      let coord = e.target.textContent
+      let yCoord = parseInt(coord[0])
+      let xCoord = parseInt(coord[1])
+
+      setHumanGB({ ...humanGB }, humanGB.placeShip(Ship(shipLength), yCoord, xCoord))
+      console.log(e.target)
+
+      setShipLenData(Array(shipLenData.length - 1).fill(1))
+    } catch (err) {
+      alert(err.message)
     }
 
-    // console.log(cpuGB)
+
   }
+
+  const checker = () => {
+    console.log(humanGB)
+  }
+  console.log(shipLenData)
 
   return (
     <div>
       <h1>BattleShip</h1>
-      <button onClick={check}>Placed Fleet?</button>
       <button onClick={start}>Start</button>
+      <button onClick={checker}>Check</button>
       <div className='activeBoards'>
-        <DisplayGameboard board={humanGB.fullBoard} />
-        <DisplayGameboard board={cpuGB.fullBoard} attack={attack} />
+        <DisplayGameboard board={humanGB} dropPlace={dropPlace} />
+        <DisplayGameboard board={cpuGB} attack={attack} />
       </div>
-      <DisplayShipYard />
+      <DisplayShipYard shipLenCheck={shipLenCheck} shipLenData={shipLenData} />
     </div>
 
   );
