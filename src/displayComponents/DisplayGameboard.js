16e41963237a24e28146cecec1b646dd90e7c062
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-06-18 18:38:37 +0100

    added ship hit and ship miss div color change

--- src/displayComponents/DisplayGameboard.js
+++ src/displayComponents/DisplayGameboard.js
@@ -2,15 +2,26 @@ import './styles/DisplayGameboard.css'
 import uniqid from 'uniqid'
 
 const DisplayGameboard = ({ board, attack, dropPlace }) => {
+    const placedShipsPositions = board.placedShips.map((ship) => ship.shipCoords).flat()
 
+    function setColor(boardArea) {
+        if (boardArea === 'Hit') {
+            return ({ backgroundColor: "red" })
+        } else if (boardArea === 'Miss') {
+            return ({ backgroundColor: "blue" })
+        } else if (placedShipsPositions.includes(boardArea)) {
+            return ({ backgroundColor: "green" })
+        }
+    }
+    console.log(board.placedShips)
     return (
         < div className="gameboard" onDragOver={(e) => e.preventDefault()} onDrop={dropPlace}>
             {board.fullBoard.map((boardArray) => {
                 return (
                     boardArray.map((boardArea) => {
                         return (
-                            <div key={uniqid()} onClick={attack} className="boardAreaDiv">
-                                {boardArea}
+                            <div key={uniqid()} id={boardArea} onClick={attack} className="boardAreaDiv" style={setColor(boardArea)}>
+
                             </div>
                         )
                     })
